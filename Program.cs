a5c48130c6e96e2f2feb942fcbb8360569c708a5
Pavel <kraspasha@mail.ru>
2023-03-21 12:08:13 +0300

    Final solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first digit: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second digit: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double i = Math.Pow(A,B);
+Console.WriteLine("Число " + A + " в степени " + B + " = " + i);
