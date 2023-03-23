3169842b5a04bbc3d1323fdd501897e6be8cf529
Pavel <kraspasha@mail.ru>
2023-03-23 13:24:20 +0300

    Final Solution

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Вводим два числа А и В и возводим число А в степень В
 Console.WriteLine("Enter first digit: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter second digit: ");
 int B = Convert.ToInt32(Console.ReadLine());
-double i = Math.Pow(A,B);
-Console.WriteLine("Число " + A + " в степени " + B + " = " + i);
+
+double PrintNum (double A1, double B1)
+{
+   double i = Math.Pow(A1,B1);
+   return i;
+}
+Console.WriteLine("Число " + A + " в степени " + B + " = " + $"{PrintNum(A, B)}");
+
