1cc16bdef171c464b1deae42e2b58e0ae56f4c03
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-23 13:35:19 +0300

    Add files via upload

---
+++ Сумма цифр числа.cs
@@ -0,0 +1,16 @@
+﻿// Вводим число и выводим сумму цифр этого числа
+Console.WriteLine("Enter number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int PrintSum(int numA)
+{
+    int Sum = 0;
+    int i = 0;
+    while (numA >= 1)
+    {
+    i = numA % 10;
+    Sum = Sum + i;
+    numA = numA/10;
+    }
+    return Sum;
+}
+Console.WriteLine("Сумма цифр числа " + A + " = " + $"{PrintSum(A)}");
