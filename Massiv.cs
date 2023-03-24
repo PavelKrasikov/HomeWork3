00016ad74b2bf9c7db9ce4806c56676cc528f3d6
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-25 00:37:45 +0300

    Add files via upload

---
+++ Massiv.cs
@@ -0,0 +1,19 @@
+﻿// Создать и вывести массив из 8 элементов
+void FillArray(int[] arr)
+{
+    for (int ind = 0; ind < arr.Length; ind++)
+    {
+        arr[ind] = new Random().Next(0, 10);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+
+int[] Mas = new int [8];
+FillArray (Mas);
+PrintArray (Mas);
